94044087885e93a31fc7aa3cb3dd3e5b74619ee1
Bopzor <26028196+Bopzor@users.noreply.github.com>
2025-09-25 14:37:06 +0200

    chore(carte): new carte

--- index.tsx
+++ index.tsx
@@ -1,26 +1,26 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-import React from 'react';
-import ReactDomServer from 'react-dom/server';
+import React from "react";
+import ReactDomServer from "react-dom/server";
 
-import Home from './src/pages/Home';
-import Presentation from './src/pages/Presentation';
-import CarteTakeAway from './src/pages/CarteTakeAway';
-import CarteRestaurant from './src/pages/CarteRestaurant';
-import Contact from './src/pages/Contact';
+import Home from "./src/pages/Home";
+import Presentation from "./src/pages/Presentation";
+import CarteTakeAway from "./src/pages/CarteTakeAway";
+import CarteRestaurant from "./src/pages/CarteRestaurant";
+import Contact from "./src/pages/Contact";
 
-const PUBLIC_DIR = path.resolve(__dirname, 'public');
-const OUT_DIR = path.resolve(__dirname, 'dist');
+const PUBLIC_DIR = path.resolve(__dirname, "public");
+const OUT_DIR = path.resolve(__dirname, "dist");
 
 fs.mkdirSync(OUT_DIR, { recursive: true });
 
 const pages = [
-  { Component: Home, filename: 'index.html' },
-  { Component: Presentation, filename: 'presentation.html' },
-  { Component: CarteTakeAway, filename: 'carte-au-poids.html' },
-  { Component: CarteRestaurant, filename: 'carte-sur-place.html' },
-  { Component: Contact, filename: 'contact.html' },
+  { Component: Home, filename: "index.html" },
+  { Component: Presentation, filename: "presentation.html" },
+  { Component: CarteTakeAway, filename: "carte-au-poids.html" },
+  { Component: CarteRestaurant, filename: "carte-sur-place.html" },
+  { Component: Contact, filename: "contact.html" },
 ];
 
 pages.forEach(({ Component, filename }) => {
@@ -43,42 +43,45 @@ pages.forEach(({ Component, filename }) => {
       <body>
         <Component />
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164069386-1" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-164069386-1"
+        />
         <script type="text/javascript" src="/assets/js/gtag.js" />
       </body>
     </html>
   );
 
-  fs.writeFileSync(path.join(OUT_DIR, filename), '<!DOCTYPE html>' + markup);
+  fs.writeFileSync(path.join(OUT_DIR, filename), "<!DOCTYPE html>" + markup);
 });
 
 const assets = [
-  'robots.txt',
-  path.join('assets', 'css', 'reset.css'),
-  path.join('assets', 'css', 'styles.css'),
-  path.join('assets', 'images', 'tea_leaf2.jpg'),
-  path.join('assets', 'images', 'flower.jpg'),
-  path.join('assets', 'images', 'tea_cup.jpg'),
-  path.join('assets', 'images', 'tea_box.jpg'),
-  path.join('assets', 'images', 'store.jpg'),
-  path.join('assets', 'images', 'logo.png'),
-  path.join('assets', 'images', 'favicon.png'),
-  path.join('assets', 'images', 'carte', 'carte-sur-place.pdf'),
-  path.join('assets', 'images', 'carte', 'carte-sur-place_p01.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-sur-place_p02.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-sur-place_p03.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-sur-place_p04.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-sur-place_p05.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-sur-place_p06.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-au-poids.pdf'),
-  path.join('assets', 'images', 'carte', 'carte-au-poids_p01.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-au-poids_p02.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-au-poids_p03.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-au-poids_p04.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-au-poids_p05.jpg'),
-  path.join('assets', 'images', 'carte', 'carte-au-poids_p06.jpg'),
-  path.join('assets', 'fonts', 'jose_fernandez', 'JoseFernandez.ttf'),
-  path.join('assets', 'js', 'gtag.js'),
+  "robots.txt",
+  path.join("assets", "css", "reset.css"),
+  path.join("assets", "css", "styles.css"),
+  path.join("assets", "images", "tea_leaf2.jpg"),
+  path.join("assets", "images", "flower.jpg"),
+  path.join("assets", "images", "tea_cup.jpg"),
+  path.join("assets", "images", "tea_box.jpg"),
+  path.join("assets", "images", "store.jpg"),
+  path.join("assets", "images", "logo.png"),
+  path.join("assets", "images", "favicon.png"),
+  path.join("assets", "images", "carte", "carte-sur-place.pdf"),
+  path.join("assets", "images", "carte", "carte-sur-place_p01.jpg"),
+  path.join("assets", "images", "carte", "carte-sur-place_p02.jpg"),
+  path.join("assets", "images", "carte", "carte-sur-place_p03.jpg"),
+  path.join("assets", "images", "carte", "carte-sur-place_p04.jpg"),
+  path.join("assets", "images", "carte", "carte-sur-place_p05.jpg"),
+  path.join("assets", "images", "carte", "carte-sur-place_p06.jpg"),
+  path.join("assets", "images", "carte", "carte-sur-place_p07.jpg"),
+  path.join("assets", "images", "carte", "carte-au-poids.pdf"),
+  path.join("assets", "images", "carte", "carte-au-poids_p01.jpg"),
+  path.join("assets", "images", "carte", "carte-au-poids_p02.jpg"),
+  path.join("assets", "images", "carte", "carte-au-poids_p03.jpg"),
+  path.join("assets", "images", "carte", "carte-au-poids_p04.jpg"),
+  path.join("assets", "images", "carte", "carte-au-poids_p05.jpg"),
+  path.join("assets", "fonts", "jose_fernandez", "JoseFernandez.ttf"),
+  path.join("assets", "js", "gtag.js"),
 ];
 
 assets.forEach((asset) => {
--- src/pages/CarteRestaurant.tsx
+++ src/pages/CarteRestaurant.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
-import Carte from '../components/Carte';
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+import Carte from "../components/Carte";
 
 const CarteRestaurant = (_: RouteComponentProps) => (
   <Carte
     page="carte-sur-place"
     pdf="/assets/images/carte/carte-sur-place.pdf"
     images={[
-      '/assets/images/carte/carte-sur-place_p01.jpg',
-      '/assets/images/carte/carte-sur-place_p02.jpg',
-      '/assets/images/carte/carte-sur-place_p03.jpg',
-      '/assets/images/carte/carte-sur-place_p04.jpg',
-      '/assets/images/carte/carte-sur-place_p05.jpg',
-      '/assets/images/carte/carte-sur-place_p06.jpg',
+      "/assets/images/carte/carte-sur-place_p01.jpg",
+      "/assets/images/carte/carte-sur-place_p02.jpg",
+      "/assets/images/carte/carte-sur-place_p03.jpg",
+      "/assets/images/carte/carte-sur-place_p04.jpg",
+      "/assets/images/carte/carte-sur-place_p05.jpg",
+      "/assets/images/carte/carte-sur-place_p06.jpg",
+      "/assets/images/carte/carte-sur-place_p07.jpg",
     ]}
   />
 );
